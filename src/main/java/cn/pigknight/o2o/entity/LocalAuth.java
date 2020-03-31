4d78248e181158578d117a3e9aa939a7868ffa61
PigKnightX <pigknightx@163.com>
2020-03-31 23:39:49 +0800

    day_01

---
+++ src/main/java/cn/pigknight/o2o/entity/LocalAuth.java
@@ -0,0 +1,60 @@
+package cn.pigknight.o2o.entity;
+
+import java.util.Date;
+
+public class LocalAuth {
+    private Long localAuthId;
+    private String username;
+    private String password;
+    private Date creaTime;
+    private Date lastEditTime;
+    private PersonInfo personInfo;
+
+    public Long getLocalAuthId() {
+        return localAuthId;
+    }
+
+    public void setLocalAuthId(Long localAuthId) {
+        this.localAuthId = localAuthId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreaTime() {
+        return creaTime;
+    }
+
+    public void setCreaTime(Date creaTime) {
+        this.creaTime = creaTime;
+    }
+
+    public Date getLastEditTime() {
+        return lastEditTime;
+    }
+
+    public void setLastEditTime(Date lastEditTime) {
+        this.lastEditTime = lastEditTime;
+    }
+
+    public PersonInfo getPersonInfo() {
+        return personInfo;
+    }
+
+    public void setPersonInfo(PersonInfo personInfo) {
+        this.personInfo = personInfo;
+    }
+}
